a2a9e8ed1d4bf558d63c9dd02752d7a2e166f76b
Hyperkopite <545018560@QQ.com>
2018-04-01 01:40:38 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,290 @@
+/** @todo (Itsuki Fujii#1#): Need to be fixed:
+1.加密结果为单行时数据过长的截断问题。 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define key_length 5
+#define mod 98
+#define mx 1000000
+
+int i, k, l, current_key = 0;
+bool is_n = false;
+char alphabet [mx] = "\n\r\t0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/\? ", keys [key_length], clr_text [mx], ciphertext [mx], c;
+int cnvrtd_clr_text [mx], cnvrtd_keys [key_length], ciphertext_num [mx], cnvrtd_ciphertext [mx], clr_text_num [mx];
+
+int chrctr_num(char c)
+{
+    for (i = 0; i < mod; i++)
+    {
+        if ((int) alphabet [i] == (int) c)
+        {
+            //printf("\nASCII No. = %d\n", (int) c);
+            return i;
+        }
+    }
+    printf("\n%c not found! ASCII No. = %d\n", c, (int) c);
+    exit(1);
+}
+
+void num_chrctr (int num)
+{
+    if (num > mod)
+    {
+        printf("Input error!\n");
+        exit(1);
+    }
+    if (num < 0)
+    {
+        num += mod;
+    }
+    ciphertext [k] = alphabet [num];
+}
+
+void num_chrctr_2 (int num)
+{
+    if (num > mod)
+    {
+        printf("Input error!\n");
+        exit(1);
+    }
+    if (num < 0)
+    {
+        num += mod;
+    }
+    clr_text [k] = alphabet [num];
+}
+
+int encrypt(int n)
+{
+    return n % mod;
+}
+
+int decrypt(int n)
+{
+    return n % mod;
+}
+
+void encrypt_circular()
+{
+    for (k = 0; k < (int) strlen(clr_text);)
+    {
+        for (l = 0; l < key_length; l++, k++)
+        {
+            if (k >= (int) strlen(clr_text))
+            {
+                break;
+            }
+            //printf("Key = %c  ", alphabet [cnvrtd_keys [l]]);
+            ciphertext_num [k] = encrypt(cnvrtd_clr_text [k] + cnvrtd_keys [l]);
+            num_chrctr(ciphertext_num [k]);
+        }
+    }
+}
+
+void decrypt_circular()
+{
+    for (k = 0; k < (int) strlen(ciphertext);)
+    {
+        for (l = is_n ? current_key : 0; l < key_length; l++, k++)
+        {
+            is_n = false;
+            if (k >= (int) strlen(ciphertext))
+            {
+                break;
+            }
+            clr_text_num [k] = decrypt(cnvrtd_ciphertext [k] - cnvrtd_keys [l]);
+            num_chrctr_2(clr_text_num [k]);
+            //printf("Current cipher = %c(%d),Key = %c(%d),Clear text = %c(%d),is_n = %s\n", alphabet [cnvrtd_ciphertext [k]], cnvrtd_ciphertext [k], alphabet [cnvrtd_keys [l]], cnvrtd_keys [l], clr_text [k], chrctr_num (clr_text [k]), is_n ? "true" : "false");
+
+            if (cnvrtd_ciphertext [k] == 0)
+            {
+                is_n = true;
+                current_key = l == key_length - 1 ? 0 : l + 1;
+                k++;
+                break;
+            }
+            if (clr_text [k] == '\n')
+            {
+                is_n = false;
+                k++;
+                break;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int op;
+    printf("Char set = %d\nCurrent KEY length = %d\nAll ASCII characters are supported\n", strlen(alphabet), key_length);
+start:
+    printf("**************************************************************\n1.Encrypt single line from manual input\n2.Decrypt single line from manual input\n3.Encrypt file text.txt under current path\n4.Decrypt file text_encrypted.txt under current path\n5.Exit\n**************************************************************\n");
+    scanf("%d", &op);
+    getchar();
+
+    switch (op)
+    {
+    case 1:
+        printf("Clear text = ");
+        gets(clr_text);
+        for (l = 0; l < (int) strlen(clr_text); l++)
+        {
+            cnvrtd_clr_text [l] = chrctr_num(clr_text [l]);
+        }
+        printf("KEY = ");
+        gets(keys);
+        for (l = 0; l < (int) strlen(keys); l++)
+        {
+            cnvrtd_keys [l] = chrctr_num(keys [l]);
+        }
+        encrypt_circular();
+        printf("Ciphertext = %s\n", ciphertext);
+        system("pause");
+        printf("\n");
+        goto start;
+
+    case 2:
+        printf("Ciphertext = ");
+        gets(ciphertext);
+        for (l = 0; l < (int) strlen(ciphertext); l++)
+        {
+            cnvrtd_ciphertext [l] = chrctr_num(ciphertext [l]);
+        }
+        printf("KEY = ");
+        gets(keys);
+        for (l = 0; l < (int) strlen(keys); l++)
+        {
+            cnvrtd_keys [l] = chrctr_num(keys [l]);
+        }
+        decrypt_circular();
+        printf("Clear text = %s\n", clr_text);
+        system("pause");
+        printf("\n");
+        goto start;
+
+    case 3:
+    {
+        FILE *fp, *fp1;
+        fp = fopen("text.txt", "r+");
+        fp1 = fopen("text_encrypted.txt", "w+");
+        if (!fp)
+        {
+            printf("Failed to open file!\n");
+            system("pause");
+            exit(1);
+        }
+        printf("KEY = ");
+        gets(keys);
+        fseek(fp,0,SEEK_END);
+        long fp_end = ftell(fp);
+        //printf("fp_end = %ld\n", fp_end);
+        fseek(fp,0,SEEK_SET);
+        //printf("Clear text = \n\n");
+        while(fgets(clr_text, mx, fp))
+        {
+            //printf("%s", clr_text);
+            long fp_current = ftell(fp);
+            printf("fp_current = %ld/fp_end = %ld\r\n", fp_current, fp_end);
+            printf("Process: %.2lf%%\r", (double) fp_current / fp_end * 100);
+            for (l = 0; l < (int) strlen(clr_text); l++)
+            {
+                cnvrtd_clr_text [l] = chrctr_num(clr_text [l]);
+            }
+
+            for (l = 0; l < (int) strlen(keys); l++)
+            {
+                cnvrtd_keys [l] = chrctr_num(keys [l]);
+            }
+            encrypt_circular();
+            fputs(ciphertext, fp1);
+            memset(ciphertext, 0, sizeof(ciphertext));
+        }
+        if (fclose(fp) != 0)
+        {
+            printf("Failed to close file\n");
+            system("pause");
+            exit(1);
+        }
+        if (fclose(fp1) != 0)
+        {
+            printf("Failed to close file\n");
+            system("pause");
+            exit(1);
+        }
+        remove("text.txt");
+        printf("\n\nDone.A \"text_encrypted.txt\" has been created under current path.\nThe \"text.txt\" has been deleted automatically.\n");
+        system("pause");
+        printf("\n");
+        goto start;
+    }
+    case 4:
+    {
+        FILE *fp2, *fp3;
+        fp2 = fopen("text_encrypted.txt", "r+");
+        fp3 = fopen("text.txt", "w+");
+        if (!fp2)
+        {
+            printf("Failed to open file!\n");
+            system("pause");
+            exit(1);
+        }
+        printf("KEY = ");
+        gets(keys);
+        //printf("Ciphertext =\n\n");
+        fseek(fp2,0,SEEK_END);
+        long fp_end2 = ftell(fp2);
+        //printf("fp_end2 = %ld\n", fp_end2);
+        fseek(fp2,0,SEEK_SET);
+        //printf("fp_current2 = %ld\n", ftell(fp2));
+        while(fgets(ciphertext, mx, fp2))
+        {
+            //printf("%s", ciphertext);
+            long fp_current2 = ftell(fp2);
+            printf("fp_current2 = %ld/fp_end2 = %ld\r\n", fp_current2, fp_end2);
+            printf("Process: %.2lf%%\r", (double) fp_current2 / fp_end2 * 100);
+
+            for (l = 0; l < (int) strlen(ciphertext); l++)
+            {
+                cnvrtd_ciphertext [l] = chrctr_num(ciphertext [l]);
+            }
+
+            for (l = 0; l < (int) strlen(keys); l++)
+            {
+                cnvrtd_keys [l] = chrctr_num(keys [l]);
+            }
+            decrypt_circular();
+            fputs(clr_text, fp3);
+            memset(clr_text, 0, sizeof(clr_text));
+        }
+
+        if (fclose(fp2) != 0)
+        {
+            printf("Failed to close file\n");
+            system("pause");
+            exit(1);
+        }
+        if (fclose(fp3) != 0)
+        {
+            printf("Failed to close file\n");
+            system("pause");
+            exit(1);
+        }
+        printf("\n\nDone.A \"text.txt\" has been recovered under current path.\n");
+        system("pause");
+        printf("\n");
+        goto start;
+    }
+
+    case 5:
+        exit(0);
+
+    default:
+        printf("Input error!\n");
+        system("pause");
+        printf("\n");
+        goto start;
+    }
+}
