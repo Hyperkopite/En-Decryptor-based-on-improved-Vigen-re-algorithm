a9332f38e6720c855b5b57ab618392117028a390
Xinyi Chen <anfieldk18@gmail.com>
2018-05-02 20:59:34 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,106 +1,176 @@
 /** @todo (Itsuki Fujii#1#): Need to be fixed:
-*need to be confirmed*1.加密结果为单行时数据过长的截断问题。???
+*fixed*1.加密结果为单行时数据过长的截断问题。???
 *fixed*2.重复进行加解密时发生的错误。
-*fixed*3.fgets()读取密文中换行后第一个字符时发生错误->该字符会被错误地读取为ASCII = 1*/
+*fixed*3.fgets()读取密文中换行后第一个字符时发生错误->该字符会被错误地读取为ASCII = 1
+*fixed*4.单行加密时密文出现换行符的问题*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #define key_length 5
-#define mod 99
+#define mod 98
+#define mod_single_line 96
 #define mx 1000000
 
-int i, k, l, current_key = 0, line = 0;
-bool is_n = false;
-char alphabet [mx] = "\n\x01\r\t0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/\? ", keys [key_length], clr_text [mx], ciphertext [mx], c;
-int cnvrtd_clr_text [mx], cnvrtd_keys [key_length], ciphertext_num [mx], cnvrtd_ciphertext [mx], clr_text_num [mx];
+int i, k, l, bytes = 1, current_key = 0, line = 1, cnvrtd_clr_text [mx], cnvrtd_keys [key_length], ciphertext_num [mx], cnvrtd_ciphertext [mx], clr_text_num [mx];
+bool is_n = false, is_single_line = false;
+char alphabet [mx] = "\x01\x0a\x0d\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e", alphabet_single_line [mx] = "\x01\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e", keys [key_length], clr_text [mx], ciphertext [mx], c;
 
 void sub()
 {
     FILE *sub;
     sub = fopen("sub.dat", "w+");
-    char restart [10] = "restart";
-    fwrite(restart, sizeof(char), sizeof(restart), sub);
+    char r [2] = "r";
+    fwrite(r, sizeof(char), sizeof(r), sub);
     if (fclose(sub) != 0)
         {
             printf("Failed to close file!\n");
             system("pause");
             exit(1);
         }
-    printf("Program has been rebooted.\n");
+    printf("Rebooting...");
     system("cipher_sub.exe");
 }
 
 int chrctr_num(char c)
 {
-    for (i = 0; i < mod; i++)
+    //printf("chrctr_num->is_single_line = %s\n", is_single_line ? "true" : "false");
+    if (!is_single_line)
         {
-            if ((int) alphabet [i] == (int) c)
+            for (i = 0; i < mod; i++)
                 {
-                    //printf("\nASCII No. = %d\n", (int) c);
-                    return i;
+                    if ((int) alphabet [i] == (int) c)
+                        {
+                            //printf("\nASCII No. = %d\n", (int) c);
+                            return i;
+                        }
                 }
+            printf("\nIllegal character detected! Ascii No.= %d, line = %d, position = %d\n", (int) c, line, bytes);
+            system("pause");
+            sub();
         }
-    printf("\n%c not found! ASCII No. = %d, line = %d\n", c, (int) c, line);
-    system("pause");
-    exit(1);
+    else
+        {
+            for (i = 0; i < mod_single_line; i++)
+                {
+                    if ((int) alphabet_single_line [i] == (int) c)
+                        {
+                            //printf("\nASCII No. = %d\n", (int) c);
+                            return i;
+                        }
+                }
+            printf("\nIllegal character detected! Ascii No.= %d, line = %d, position = %d\n", (int) c, line, bytes);
+            system("pause");
+            sub();
+        }
 }
 
-void check(char c)
-{
-    if ((int) c == 1)
-        {
+//void check(char c)
+//{
+//    if ((int) c == 1)
+//        {
 //            printf("got 1!\n");
 //            system("pause");
-        }
+//        }
 //    else
 //        {
 //            printf("not 1!\n");
 //        }
-}
+//}
 
 void num_chrctr (int num)
 {
-    if (num > mod)
+    //printf("num_chrctr->is_single_line = %s\n", is_single_line ? "true" : "false");
+    if (!is_single_line)
         {
-            printf("Input error!\n");
-            exit(1);
+            if (num > mod)
+                {
+                    printf("Input error!\n");
+                    exit(1);
+                }
+            if (num < 0)
+                {
+                    num += mod;
+                }
+            ciphertext [k] = alphabet [num];
         }
-    if (num < 0)
+    else
         {
-            num += mod;
+            if (num > mod_single_line)
+                {
+                    printf("Input error!\n");
+                    exit(1);
+                }
+            if (num < 0)
+                {
+                    num += mod_single_line;
+                }
+            ciphertext [k] = alphabet_single_line [num];
         }
-    ciphertext [k] = alphabet [num];
-    check(ciphertext [k]);
+    //check(ciphertext [k]);
 }
 
 void num_chrctr_2 (int num)
 {
-    if (num > mod)
+    //printf("num_chrctr_2->is_single_line = %s\n", is_single_line ? "true" : "false");
+    if (!is_single_line)
         {
-            printf("Input error!\n");
-            exit(1);
+            if (num > mod)
+                {
+                    printf("Input error!\n");
+                    exit(1);
+                }
+            if (num < 0)
+                {
+                    num += mod;
+                }
+            clr_text [k] = alphabet [num];
         }
-    if (num < 0)
+    else
         {
-            num += mod;
+            if (num > mod_single_line)
+                {
+                    printf("Input error!\n");
+                    exit(1);
+                }
+            if (num < 0)
+                {
+                    num += mod_single_line;
+                }
+            clr_text [k] = alphabet_single_line [num];
         }
-    clr_text [k] = alphabet [num];
 }
 
 int encrypt(int n)
 {
-    return n % mod;
+    //printf("encrypt->is_single_line = %s\n", is_single_line ? "true" : "false");
+    if (!is_single_line)
+        {
+            return n % mod;
+        }
+    else
+        {
+            return n % mod_single_line;
+        }
 }
 
 int decrypt(int n)
 {
-    return n % mod;
+    //printf("decrypt->is_single_line = %s\n", is_single_line ? "true" : "false");
+    if (!is_single_line)
+        {
+            return n % mod;
+        }
+    else
+        {
+            return n % mod_single_line;
+        }
 }
 
 void encrypt_circular()
 {
+    //printf("encrypt_circular->is_single_line = %s\n", is_single_line ? "true" : "false");
     for (k = 0; k < (int) strlen(clr_text);)
         {
             for (l = 0; l < key_length; l++, k++)
@@ -110,14 +180,25 @@ void encrypt_circular()
                             break;
                         }
                     ciphertext_num [k] = encrypt(cnvrtd_clr_text [k] + cnvrtd_keys [l]);
-                    //printf("\nClear text = %c(%d),Key = %c(%d),Cipher = %c(%d)\n", alphabet [cnvrtd_clr_text [k]], cnvrtd_clr_text [k], alphabet [cnvrtd_keys [l]], cnvrtd_keys [l], alphabet [ciphertext_num [k]], ciphertext_num [k]);
+
+//                    if (is_single_line)
+//                        {
+//                            printf("\nClear text = %c(%d),Key = %c(%d),Cipher = %c(%d)\n", alphabet_single_line [cnvrtd_clr_text [k]], cnvrtd_clr_text [k], alphabet_single_line [cnvrtd_keys [l]], cnvrtd_keys [l], alphabet_single_line [ciphertext_num [k]], ciphertext_num [k]);
+//                        }
+//                    else
+//                        {
+//                            printf("\nClear text = %c(%d),Key = %c(%d),Cipher = %c(%d)\n", alphabet [cnvrtd_clr_text [k]], cnvrtd_clr_text [k], alphabet [cnvrtd_keys [l]], cnvrtd_keys [l], alphabet [ciphertext_num [k]], ciphertext_num [k]);
+//                        }
+
                     num_chrctr(ciphertext_num [k]);
                 }
         }
+        //printf("Ciphertext = %s\n", ciphertext);
 }
 
 void decrypt_circular()
 {
+    //printf("decrypt_circular->is_single_line = %s\n", is_single_line ? "true" : "false");
     for (k = 0; k < (int) strlen(ciphertext);)
         {
             for (l = is_n ? current_key : 0; l < key_length; l++, k++)
@@ -131,7 +212,7 @@ void decrypt_circular()
                     clr_text_num [k] = decrypt(cnvrtd_ciphertext [k] - cnvrtd_keys [l]);
                     num_chrctr_2(clr_text_num [k]);
 
-                    if (ciphertext [k] == '\n')
+                    if (ciphertext [k] == '\x0a')
                         {
                             is_n = true;
                             current_key = l == key_length - 1 ? 0 : l + 1;
@@ -139,9 +220,16 @@ void decrypt_circular()
 //                            break;
                         }
 
-                    //printf("Current cipher = %c(%d),Key = %c(%d),Clear text = %c(%d),is_n = %s\n", alphabet [cnvrtd_ciphertext [k]], cnvrtd_ciphertext [k], alphabet [cnvrtd_keys [l]], cnvrtd_keys [l], clr_text [k], chrctr_num (clr_text [k]), is_n ? "true" : "false");
+//                    if (!is_single_line)
+//                    {
+//                        printf("Current cipher = %c(%d),Key = %c(%d),Clear text = %c(%d),is_n = %s\n", alphabet [cnvrtd_ciphertext [k]], cnvrtd_ciphertext [k], alphabet [cnvrtd_keys [l]], cnvrtd_keys [l], clr_text [k], chrctr_num (clr_text [k]), is_n ? "true" : "false");
+//                    }
+//                    else
+//                    {
+//                        printf("Current cipher = %c(%d),Key = %c(%d),Clear text = %c(%d),is_n = %s\n", alphabet_single_line [cnvrtd_ciphertext [k]], cnvrtd_ciphertext [k], alphabet_single_line [cnvrtd_keys [l]], cnvrtd_keys [l], clr_text [k], chrctr_num (clr_text [k]), is_n ? "true" : "false");
+//                    }
 
-                    if (clr_text [k] == '\n')
+                    if (clr_text [k] == '\x0a')
                         {
                             is_n = false;
                             k++;
@@ -150,28 +238,30 @@ void decrypt_circular()
                 }
         }
 }
-// no c, no n, no h,
 
 int main()
 {
+    //printf("%c\n", alphabet_single_line [14]);
     int op;
-    printf("Char set = %d\nCurrent KEY length = %d\nAll ASCII characters are supported\n", strlen(alphabet), key_length);
-    line = 0;
-    printf("**************************************************************\n1.Encrypt single line from manual input\n2.Decrypt single line from manual input\n3.Encrypt file text.txt under current path\n4.Decrypt file text_encrypted.txt under current path\n5.Exit\n**************************************************************\n");
+    printf("Char set = %d\nCurrent key length = %d\nAll displayable ascii characters are supported\n", strlen(alphabet), key_length);
+    printf("===============================================================\n1.Encrypt single line from manual input\n2.Decrypt single line from manual input\n3.Encrypt file text.txt under current path\n4.Decrypt file text_encrypted.txt under current path\n5.Exit\n===============================================================\n");
     scanf("%d", &op);
     getchar();
 
     switch (op)
         {
         case 1:
+            is_single_line = true;
             printf("Clear text = ");
             gets(clr_text);
             for (l = 0; l < (int) strlen(clr_text); l++)
                 {
                     cnvrtd_clr_text [l] = chrctr_num(clr_text [l]);
+                    bytes++;
                 }
             printf("KEY = ");
             gets(keys);
+            is_single_line = true;
             for (l = 0; l < (int) strlen(keys); l++)
                 {
                     cnvrtd_keys [l] = chrctr_num(keys [l]);
@@ -183,14 +273,17 @@ int main()
             sub();
 
         case 2:
+            is_single_line = true;
             printf("Ciphertext = ");
             gets(ciphertext);
             for (l = 0; l < (int) strlen(ciphertext); l++)
                 {
                     cnvrtd_ciphertext [l] = chrctr_num(ciphertext [l]);
+                    bytes++;
                 }
             printf("KEY = ");
             gets(keys);
+            is_single_line = true;
             for (l = 0; l < (int) strlen(keys); l++)
                 {
                     cnvrtd_keys [l] = chrctr_num(keys [l]);
@@ -221,14 +314,15 @@ int main()
             //printf("Clear text = \n\n");
             while(fgets(clr_text, mx, fp))
                 {
+                    bytes = 1;
                     //printf("%s", clr_text);
                     long fp_current = ftell(fp);
-                    line++;
                     //printf("fp_current = %ld/fp_end = %ld\r\n", fp_current, fp_end);
                     printf("Process: %.2lf%%\r", (double) fp_current / fp_end * 100);
                     for (l = 0; l < (int) strlen(clr_text); l++)
                         {
                             cnvrtd_clr_text [l] = chrctr_num(clr_text [l]);
+                            bytes++;
                         }
 
                     for (l = 0; l < (int) strlen(keys); l++)
@@ -237,6 +331,7 @@ int main()
                         }
                     encrypt_circular();
                     fputs(ciphertext, fp1);
+                    line++;
                     memset(ciphertext, 0, sizeof(ciphertext));
                 }
             if (fclose(fp) != 0)
@@ -278,15 +373,16 @@ int main()
             //printf("fp_current2 = %ld\n", ftell(fp2));
             while(fgets(ciphertext, mx, fp2))
                 {
+                    bytes = 1;
                     //printf("%s", ciphertext);
                     long fp_current2 = ftell(fp2);
-                    line++;
                     //printf("fp_current2 = %ld/fp_end2 = %ld\r\n", fp_current2, fp_end2);
                     printf("Process: %.2lf%%\r", (double) fp_current2 / fp_end2 * 100);
 
                     for (l = 0; l < (int) strlen(ciphertext); l++)
                         {
                             cnvrtd_ciphertext [l] = chrctr_num(ciphertext [l]);
+                            bytes++;
                         }
 
                     for (l = 0; l < (int) strlen(keys); l++)
@@ -295,6 +391,7 @@ int main()
                         }
                     decrypt_circular();
                     fputs(clr_text, fp3);
+                    line++;
                     memset(clr_text, 0, sizeof(clr_text));
                 }
 
